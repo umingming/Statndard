1edb58e4d541bdf86f0b1424d33bfa2381e88916
umingming <u_0112@naver.com>
2021-09-27 22:47:48 +0900

    break

---
+++ Standard/src/com/standard/ch4/FlowEx30.java
@@ -0,0 +1,20 @@
+package com.standard.ch4;
+
+public class FlowEx30 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		while(true) {
+			if(sum > 100)
+				break;
+			++i;
+			sum += i;
+		}
+		
+		System.out.println(i);
+		System.out.println(sum);
+	}
+
+}
