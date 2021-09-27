f4c90956af17fb7adcae1848e8a1096d2aea806e
umingming <u_0112@naver.com>
2021-09-27 22:20:54 +0900

    누적합계

--- Standard/src/com/standard/ch4/FlowEx26.java
+++ Standard/src/com/standard/ch4/FlowEx26.java
@@ -3,7 +3,13 @@ package com.standard.ch4;
 public class FlowEx26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int sum = 0;
+		int i = 0;
+		
+		while((sum += ++i) <= 100) {
+			System.out.print(i + " ");
+			System.out.println(sum);
+		}
 
 	}
 
