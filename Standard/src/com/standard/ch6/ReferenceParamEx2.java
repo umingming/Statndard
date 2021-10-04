671fd4dccf453b006392ceaf4c42674774799be1
umingming <u_0112@naver.com>
2021-10-04 19:40:01 +0900

    참조형 매개변수

---
+++ Standard/src/com/standard/ch6/ReferenceParamEx2.java
@@ -0,0 +1,18 @@
+package com.standard.ch6;
+
+public class ReferenceParamEx2 {
+
+	public static void main(String[] args) {
+		int[] x = {10};
+		System.out.println(x[0]);
+		change(x);
+		System.out.println(x[0]);
+
+	}
+	
+	static void change(int[] x) {
+		x[0] = 1000;
+		System.out.println(x[0]);
+	}
+
+}
