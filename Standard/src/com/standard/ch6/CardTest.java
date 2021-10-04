ca12d5120d62bbf621420e032c1dc9c28b9caa82
umingming <u_0112@naver.com>
2021-10-04 19:09:42 +0900

    수정

--- Standard/src/com/standard/ch6/CardTest.java
+++ Standard/src/com/standard/ch6/CardTest.java
@@ -19,6 +19,7 @@ public class CardTest {
 		
 		c1.width = 50;
 		c1.height = 80;
+
 		System.out.println(c1.kind + c1.number + " " + c1.height + ", " + c1.width);
 		System.out.println(c2.kind + c2.number + " " + c2.height + ", " + c2.width);
 	}
