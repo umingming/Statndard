690aaf783fd5d3c12ff3163b26c7c2c35996c1ca
umingming <u_0112@naver.com>
2021-10-13 01:00:01 +0900

    카드 클래스 주석 처리

--- Standard/src/com/standard/ch7/DeckTest.java
+++ Standard/src/com/standard/ch7/DeckTest.java
@@ -48,17 +48,18 @@ class Deck{
 }
 
 
-class Card{
-	static final int KIND_MAX = 4;
-	static final int NUM_MAX = 13;
-
-	static final int SPADE = 4;
-	static final int DIAMOND = 3;
-	static final int HEART = 2;
-	static final int CLOVER = 1;
+//class Card{
+//	static final int KIND_MAX = 4;
+//	static final int NUM_MAX = 13;
+//
+//	static final int SPADE = 4;
+//	static final int DIAMOND = 3;
+//	static final int HEART = 2;
+//	static final int CLOVER = 1;
 	
-	int kind;
-	int number;
+	final String KIND;
+	final int NUMBER;
+	static int width
 	
 	Card(){
 		this(SPADE, 1);
