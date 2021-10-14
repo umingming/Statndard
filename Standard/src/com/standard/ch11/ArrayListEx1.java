91b77ff9d46f37f156f4dd4cda48a9922f592baa
umingming <u_0112@naver.com>
2021-10-14 19:53:11 +0900

    긴 문자열 ArrayList에 담기

--- Standard/src/com/standard/ch11/ArrayListEx1.java
+++ Standard/src/com/standard/ch11/ArrayListEx1.java
@@ -16,21 +16,23 @@ public class ArrayListEx1 {
 		
 		ArrayList list2 = new ArrayList(list1.subList(1, 4));
 		print(list1, list2);
-		
+		System.out.println("-");
+		System.out.println(list1);
 		Collections.sort(list1);
 		Collections.sort(list2);
 		print(list1, list2);
 		
 		System.out.println(list1.containsAll(list2)); //true
 		
-		list2.add("B");
 		list2.add("C");
+		list2.add("B");
 		list2.add(3, "A"); //024ABC
 		print(list1, list2);
 		
 		list2.set(3, "AA"); //024AABC
 		print(list1, list2);
 		
+	
 		System.out.println(list1.retainAll(list2)); //024
 		print(list1, list2);
 		
@@ -39,6 +41,8 @@ public class ArrayListEx1 {
 				list2.remove(i);
 			}
 		}
+		
+		Collections.sort(list2);
 		print(list1, list2);
 		
 	}
