b1c7cc2fa76d200ae9ac6931c26d7fb80111435d
umingming <u_0112@naver.com>
2021-10-14 16:40:54 +0900

    try-catch문 사용

---
+++ Standard/src/com/standard/ch8/ExceptionEx1.java
@@ -0,0 +1,18 @@
+package com.standard.ch8;
+
+public class ExceptionEx1 {
+
+	public static void main(String[] args) {
+//		try {
+//			try { } catch (Exception e) { }
+//		} catch (Exception e) {
+//			try { } catch (Exception e) { }
+//		}
+		
+		try {
+		} catch (Exception e) {
+		}
+
+	}
+
+}
---
+++ Standard/src/com/standard/ch8/package-info.java
@@ -0,0 +1,1 @@
+package com.standard.ch8;
