6f64b78dcb96ac838f604c4556b45920cd9d775e
umingming <u_0112@naver.com>
2021-10-14 17:00:41 +0900

    위의 catch블럭에서 예외가 처리되면 다음 catch 블럭은 검사하지 않음

--- Standard/src/com/standard/ch8/ExceptionEx4.java
+++ Standard/src/com/standard/ch8/ExceptionEx4.java
@@ -9,8 +9,13 @@ public class ExceptionEx4 {
 			System.out.println(3);
 			System.out.println(0/0);
 			System.out.println(4);
-		} catch (ArithmeticException e) {
-			System.out.println(5);
+		} catch (ArithmeticException ae) {
+			if(ae instanceof ArithmeticException){
+				System.out.println("true");
+			}
+			System.out.println("ArithmeticException");
+		} catch (Exception e) {
+			System.out.println("Exception");
 		}
 		
 		System.out.println(6);
