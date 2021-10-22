816296bb519f3a4f99bd164b135a1d9a4a024e54
umingming <u_0112@naver.com>
2021-10-22 18:31:01 +0900

    this is ASKII

---
+++ Standard/src/com/standard/ch11/AsciiPrint.java
@@ -0,0 +1,13 @@
+package com.standard.ch11;
+
+public class AsciiPrint {
+
+	public static void main(String[] args) {
+		char ch = ' ';
+		for(int i=0; i<95; i++) {
+			System.out.print(ch++);
+		}
+
+	}
+
+}
--- Standard/src/com/standard/ch11/TreeSetEx1.java
+++ Standard/src/com/standard/ch11/TreeSetEx1.java
@@ -7,17 +7,17 @@ public class TreeSetEx1 {
 	public static void main(String[] args) {
 		TreeSet set = new TreeSet();
 		
-		String from = "b";
-		String to = "d";
+		String from = "나";
+		String to = "라";
 		
 		set.add("abc");
 		set.add("car");
 		set.add("sdf");
-		set.add("gsd");
-		set.add("ewggw");
+		set.add("나다라");
+		set.add("앙ㅎ대");
 		set.add("sdgsgd");
-		set.add("sgd");
-		set.add("esgd");
+		set.add("라라라");
+		set.add("라");
 		set.add("dsge");
 		set.add("ges");
 		set.add("ges");
